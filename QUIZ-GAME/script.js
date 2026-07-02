6d8ca751a62d0129ab40795e3d7e2cde7376f8c6
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-07-02 22:42:46 +0530

    fix: correct button ID and improve result screen layout for quiz game and add the all logics

--- QUIZ-GAME/script.js
+++ QUIZ-GAME/script.js
@@ -14,8 +14,6 @@ const resultMessage = document.getElementById("result-message");
 const restartButton = document.getElementById("restart-btn");
 const progressBar = document.getElementById("progress");
 
-
-
 //! Quiz questions
 const quizQuestions = [
   {
@@ -64,3 +62,96 @@ const quizQuestions = [
     ],
   },
 ];
+
+//! quiz state variables
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+totalQuestionsSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+
+//! ADD event listeners
+startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+
+function startQuiz() {
+  console.log("start quiz");
+  currentQuestionIndex = 0;
+  scoreSpan.textContent = 0;
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+  showQuestion();
+}
+
+function showQuestion() {
+  answersDisabled = false;
+  const currentQuestion = quizQuestions[currentQuestionIndex];
+  currentQuestionSpan.textContent = currentQuestionIndex + 1;
+  const progresspercentage =
+    (currentQuestionIndex / quizQuestions.length) * 100;
+  progressBar.style.width = `${progresspercentage}%`;
+  questionText.textContent = currentQuestion.question;
+  answersContainer.innerHTML = "";
+  currentQuestion.answers.forEach((answer, index) => {
+    const button = document.createElement("button");
+    button.textContent = answer.text;
+    button.classList.add("answer-btn");
+    // what is dataset? it's a property of the button element that allows you to store custom data
+    button.dataset.correct = answer.correct;
+
+    button.addEventListener("click", () => selectAnswer());
+    answersContainer.appendChild(button);
+  });
+}
+
+function selectAnswer() {
+  if (answersDisabled) return;
+  const selectedAnswer = event.target;
+  const correctAnswer = selectedAnswer.dataset.correct;
+  Array.from(answersContainer.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    } else {
+      button.classList.add("incorrect");
+    }
+  });
+  if (correctAnswer === "true") {
+    score++;
+    scoreSpan.textContent = score;
+  }
+
+  setTimeout(() => {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < quizQuestions.length) {
+      showQuestion();
+    } else {
+      ShowResult();
+    }
+  }, 1000);
+}
+
+function ShowResult() {
+  quizScreen.classList.remove("active");
+  resultScreen.classList.add("active");
+  finalScoreSpan.textContent = score;
+  const percentage = (score / quizQuestions.length) * 100;
+
+  if (percentage === 100) {
+    resultMessage.textContent = "Perfect! You're a genius!";
+  } else if (percentage >= 80) {
+    resultMessage.textContent = "Great job! You know your stuff!";
+  } else if (percentage >= 60) {
+    resultMessage.textContent = "Good effort! Keep learning!";
+  } else if (percentage >= 40) {
+    resultMessage.textContent = "Not bad! Try again to improve!";
+  } else {
+    resultMessage.textContent = "Keep studying! You'll get better!";
+  }
+}
+
+function restartQuiz() {
+  console.log("restart quiz");
+  resultScreen.classList.remove("active");
+  startQuiz();
+}
