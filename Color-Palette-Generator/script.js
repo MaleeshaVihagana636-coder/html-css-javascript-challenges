4e02e1da16b072d1d2a4327a20def047128418f3
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-07-05 20:18:35 +0530

    add: implement color generation functionality on button click

--- Color-Palette-Generator/script.js
+++ Color-Palette-Generator/script.js
@@ -1,2 +1,20 @@
 const generateBtn = document.querySelector("#generate-btn");
 const paletteContainer = document.querySelector(".palettes-container");
+
+
+generateBtn.addEventListener("click",generatePalette)
+
+function generatePalette(){
+    let color = [];
+    for (let i = 0; i < 5; i++) {
+        color.push(generateRandomColor())
+    }
+};
+function generateRandomColor(){
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+}
+generatePalette();
