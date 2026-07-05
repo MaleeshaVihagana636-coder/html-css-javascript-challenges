cb639ecab9258fe0d44deecc0fd7b28f76a70ca9
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-07-05 20:27:26 +0530

    fix: correct palette display update logic and ensure color boxes are populated

--- Color-Palette-Generator/script.js
+++ Color-Palette-Generator/script.js
@@ -9,6 +9,8 @@ function generatePalette(){
     for (let i = 0; i < 5; i++) {
         color.push(generateRandomColor())
     }
+
+    updatePaletteDisplay(color);
 };
 function generateRandomColor(){
     const letters = "0123456789ABCDEF";
@@ -16,5 +18,19 @@ function generateRandomColor(){
     for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
 }
+
+function updatePaletteDisplay(colors){
+    const ColorBox = document.querySelector(".color-box");
+    ColorBox.forEach((colorBox, index) => {
+        const color = colors[index];
+        const colorDiv =  document.querySelector(".color");
+        const colorCode = document.querySelector(".color-code");
+        colorDiv.style.backgroundColor = color;
+        colorCode.textContent = color;
+    })
+    paletteContainer.innerHTML = "";
+}
+
 generatePalette();
