f028716f96890f4dcbc378bb2ff0bbae8ad57f0c
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-07-20 00:32:53 +0530

    style: add drag event listeners for card elements to enable drag-and-drop functionality

--- DRAG-AND-DROP/script.js
+++ DRAG-AND-DROP/script.js
@@ -1,3 +1,7 @@
 const Cards = document.querySelectorAll(".card");
 const Lists = document.querySelectorAll(".list");
 
+for (const card of Cards) {
+  card.addEventListener("dragstart", dragStart);
+  card.addEventListener("dragend", dragEnd);
+}
