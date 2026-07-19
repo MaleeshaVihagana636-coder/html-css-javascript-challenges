7fa89ca79bd738bb748f0fb8aba24c9f1859e4b1
Maleesha Vihagana <maleeshavihagana636@gmail.com>
2026-07-20 00:33:13 +0530

    style: add drag event listeners for list elements to complete drag-and-drop functionality

--- DRAG-AND-DROP/script.js
+++ DRAG-AND-DROP/script.js
@@ -5,3 +5,10 @@ for (const card of Cards) {
   card.addEventListener("dragstart", dragStart);
   card.addEventListener("dragend", dragEnd);
 }
+
+for (const list of Lists) {
+  list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("drop", dragDrop);
+}
